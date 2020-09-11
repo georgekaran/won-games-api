369caeced51f2c75f930fd04de6249f1ecf35866
georgekaran <georgekaran12@gmail.com>
2020-09-11 17:09:16 -0300

    refactor: remove SocialLinks from HomePage

---
+++ admin/src/containers/HomePage/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable */
+import React, { memo } from "react";
+import { FormattedMessage } from "react-intl";
+import PageTitle from "../../components/PageTitle";
+
+import {
+  Block,
+  Container,
+  LinkWrapper,
+  P,
+  Separator,
+} from "./components";
+
+const FIRST_BLOCK_LINKS = [
+  {
+    link:
+      "https://strapi.io/documentation/v3.x/getting-started/quick-start.html#_4-create-a-category-content-type",
+    contentId: "app.components.BlockLink.documentation.content",
+    titleId: "app.components.BlockLink.documentation",
+  },
+  {
+    link: "https://github.com/strapi/foodadvisor",
+    contentId: "app.components.BlockLink.code.content",
+    titleId: "app.components.BlockLink.code",
+  },
+];
+
+const HomePage = () => {
+  return (
+    <>
+      <FormattedMessage id="HomePage.helmet.title">
+        {(title) => <PageTitle title={title} />}
+      </FormattedMessage>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-lg-12 col-md-12">
+            <Block>
+              <h2 id="mainHeader">Bem vindo a Won Games!</h2>
+              <P>
+                Ao lado você pode inserir diferentes jogos, categorias e
+                publishers para a nossa maravilhosa loja de jogos!{" "}
+              </P>
+              <Separator style={{ marginTop: 37, marginBottom: 36 }} />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                {FIRST_BLOCK_LINKS.map((data, index) => {
+                  const type = index === 0 ? "doc" : "code";
+
+                  return (
+                    <LinkWrapper
+                      href={data.link}
+                      target="_blank"
+                      key={data.link}
+                      type={type}
+                    >
+                      <FormattedMessage id={data.titleId}>
+                        {(title) => <p className="bold">{title}</p>}
+                      </FormattedMessage>
+                      <FormattedMessage id={data.contentId}>
+                        {(content) => <p>{content}</p>}
+                      </FormattedMessage>
+                    </LinkWrapper>
+                  );
+                })}
+              </div>
+            </Block>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
