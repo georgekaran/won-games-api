bb1f55b4ee1c54356e2ba7170ed11c1d019df79c
georgekaran <georgekaran12@gmail.com>
2020-09-12 18:05:47 -0300

    feat: add game populate service

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -5,4 +5,18 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async populate(ctx) {
+    console.log('Starting to populate...')
+
+    const options = {
+      page: '1',
+      sort: 'popularity',
+      ...ctx.query
+    }
+
+    await strapi.services.game.populate(options)
+
+    ctx.send('Finished populating!')
+  }
+};
